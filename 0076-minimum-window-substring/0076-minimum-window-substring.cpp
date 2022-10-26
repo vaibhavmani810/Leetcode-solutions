af3d0571bb2ee8bf58dc572f2ff3a8b592707fcd
vaibhavmani810 <74287392+vaibhavmani810@users.noreply.github.com>
2022-10-26 14:56:25 +0530

    Time: 61 ms (23.54%), Space: 8 MB (30.50%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        unordered_map<char,int> mp1,mp2;
+        for(auto x:t)mp1[x]++;
+        int res=(int)mp1.size();
+        int start=0;
+        int ans=INT_MAX;
+        set<char> st;
+        int b=INT_MAX,e=INT_MAX;
+        for(int i=0;i<s.size();i++)
+        {
+            mp2[s[i]]++;
+            if(mp2[s[i]]==mp1[s[i]])
+            {
+                st.insert(s[i]);
+            }
+            if(mp1.find(s[i])!=mp1.end())
+            {
+                while(mp1[s[start]]<mp2[s[start]]&&st.size()==res)
+                {
+                    mp2[s[start]]--;
+                    start++;
+                }
+            }
+            if(st.size()==res && ans>(i-start+1))
+            {
+                ans=min(ans,i-start+1);
+                b=start;
+                e=i;
+                
+            }
+        }
+        if(b==INT_MAX)return "";
+        return s.substr(b,e-b+1);
+    }
+};
