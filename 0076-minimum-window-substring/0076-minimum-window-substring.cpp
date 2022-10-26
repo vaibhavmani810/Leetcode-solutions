d4904b0fce0fcfe9276b1616fab8ceb9270f21ee
vaibhavmani810 <74287392+vaibhavmani810@users.noreply.github.com>
2022-10-26 14:57:02 +0530

    Time: 71 ms (19.45%), Space: 7.8 MB (42.25%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -6,24 +6,24 @@ public:
         int res=(int)mp1.size();
         int start=0;
         int ans=INT_MAX;
-        set<char> st;
+        int cnt=0;
         int b=INT_MAX,e=INT_MAX;
         for(int i=0;i<s.size();i++)
         {
             mp2[s[i]]++;
             if(mp2[s[i]]==mp1[s[i]])
             {
-                st.insert(s[i]);
+                cnt++;
             }
             if(mp1.find(s[i])!=mp1.end())
             {
-                while(mp1[s[start]]<mp2[s[start]]&&st.size()==res)
+                while(mp1[s[start]]<mp2[s[start]]&&cnt==res)
                 {
                     mp2[s[start]]--;
                     start++;
                 }
             }
-            if(st.size()==res && ans>(i-start+1))
+            if(cnt==res && ans>(i-start+1))
             {
                 ans=min(ans,i-start+1);
                 b=start;
